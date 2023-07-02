3075b2be5fee33da081977d3e8db0493da40ff01
Piyush More <piyushmore109@gmail.com>
2023-07-02 23:14:55 +0530

    ChitChat Login Page UI

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,3 +1,5 @@
+import "package:chitchat/components/my_button.dart";
+import "package:chitchat/components/my_text_field.dart";
 import "package:flutter/material.dart";
 
 class LoginPage extends StatefulWidget {
@@ -8,15 +10,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Piyush More",),
-        ],
+      backgroundColor: Colors.grey[200],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'images/ChitChatLogo.png',
+                  height: 80,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  "Welcome back buddy you\'ve been missed!",
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                MyTextField(
+                  controller: emailController,
+                  hintText: "Enter your email",
+                  obscureText: false,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                MyButton(
+                  onTap: () {},
+                  text: "S I G N  I N",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Not a member? "),
+                    Text(
+                      "Register Now",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      
     );
   }
 }
