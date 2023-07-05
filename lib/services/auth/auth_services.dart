0e3df563c5f292efc6cb91922915dd173b796019
Piyush More <piyushmore109@gmail.com>
2023-07-05 22:59:44 +0530

    cloud FireStore added

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthServices extends ChangeNotifier {
   //User instance
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   //Signin method
   Future<UserCredential> signInWithEmailandPassword(
@@ -14,6 +16,11 @@ class AuthServices extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid' : userCredential.user!.uid,
+        'email' : email,
+      }, SetOptions(merge: true));
       return userCredential;
     } on FirebaseAuthException catch (error) {
       throw Exception(error.code);
@@ -34,6 +41,14 @@ class AuthServices extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid' : userCredential.user!.uid,
+        'email' : email,
+      });
+
+
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
